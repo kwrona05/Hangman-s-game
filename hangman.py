fcdaee615d746b524f385a81b2fe2b58b9e3cad0
kwrona05 <kwrona05@gmail.com>
2025-02-15 23:51:35 +0100

    commit

--- hangman.py
+++ hangman.py
@@ -1,22 +1,4 @@
 import random
 
 words = ['computer', 'pc', 'mouse', 'keyboard', 'monitor', 'microphone', 'headphones']
-selected_chars = []
-print('Welcome in hangman game')
-while True:
-    start_word = input("Enter 'start' for run the game")
-
-    if start_word == 'start':
-        word_to_guess = random.choice(words)
-
-        if users_word == word_to_guess:
-            print('You win')
-        else:
-            for char in word_to_guess:
-                users_char = input('Enter the char')
-                if users_char in word_to_guess:
-                    index = word_to_guess.index(users_char)
-                    print(f'Congrats, this char is {index}')
-                else:
-                    print("Nah, this char isn'n in this word. Try again")
-        
+points = 5
